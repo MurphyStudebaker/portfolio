65b004be205a5dae16d245686081b01b5e5d8a26
Murphy Studebaker <murphy.stude@gmail.com>
2023-06-12 22:12:39 -0700

    escapes for apostrophes

--- components/Heading.js
+++ components/Heading.js
@@ -2,13 +2,13 @@ export default function Heading() {
   return (
     <section>
       <h1 className="font-black text-7xl">
-        <span className="text-green-600">I'm Murphy,</span>
+        <span className="text-green-600">I&apos;m Murphy,</span>
         <br />a creative software engineer
         <span className="text-green-600 animate-spin">*</span>in Los Angeles
       </h1>
       <div className="h-6" />
       <h2 className="text-xl">
-        <span className="text-green-600">*</span>I'm also a
+        <span className="text-green-600">*</span>I&apos;m also a
         <a href="" className="transition-colors hover:text-green-600">
           songwriter
         </a>
@@ -25,11 +25,11 @@ export default function Heading() {
       <p className="text-3xl">
         I currently
         <span className="text-green-600">teach computer science full-time</span>
-        at Chapman University's Fowler School of Engineering
+        at Chapman University&apos;s Fowler School of Engineering
         <span className="text-green-600">
           while getting my Masters in Computer Science
         </span>
-        from Georgia Tech's OMSCS
+        from Georgia Tech&apos;s OMSCS
       </p>
       <div className="h-6" />
 
