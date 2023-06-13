88ea94ee7e7c93fb5c6c70a9286dd90e83402604
Murphy Studebaker <murphy.stude@gmail.com>
2023-06-13 09:45:06 -0700

    added links to all of the projects

---
+++ components/Splash.js
@@ -0,0 +1,77 @@
+export default function Splash() {
+  return (
+    <section>
+      <h1 className="font-black text-7xl">
+        <span className="text-green-600">I&apos;m Murphy,</span>
+        <br /> a creative software engineer
+        <span className="text-green-600">*</span> in Los Angeles
+      </h1>
+      <div className="h-6" />
+      <h2 className="text-xl">
+        <span className="text-green-600">*</span>I&apos;m also a{" "}
+        <a
+          href="https://open.spotify.com/artist/2CDcnaCbLqaJBTDxFlaoXj"
+          className="transition-colors hover:text-green-600"
+        >
+          songwriter & musician
+        </a>
+        ,
+        <a
+          className="transition-colors hover:text-green-600"
+          href="https://medium.com/@murphystude/how-i-built-a-home-in-the-backseat-of-my-honda-fit-83070a256a6d"
+        >
+          {" "}
+          amateur carpenter
+        </a>
+        , and (retired){" "}
+        <a
+          className="transition-colors hover:text-green-600"
+          href="https://www.youtube.com/channel/UCVT1XES1u_NJGQjznODQpMA?view_as=subscriber"
+        >
+          Harry Potter YouTuber
+        </a>
+      </h2>
+      <div className="h-24" />
+      <p className="text-3xl">
+        I currently{" "}
+        <span className="text-green-600">
+          teach computer science full-time{" "}
+        </span>
+        at Chapman University&apos;s Fowler School of Engineering{" "}
+        <span className="text-green-600">while getting my Masters </span>
+        in Computer Science at Georgia Tech & building side projects
+      </p>
+      <div className="h-6" />
+
+      <p className="text-xl text-green-600">
+        Previously: Data Scientist II at The Walt Disney Company
+      </p>
+
+      <div className="h-24" />
+      <p className="text-xl">
+        Things I&apos;ve Built:{" "}
+        <a
+          className="transition-colors hover:text-green-600"
+          href="https://inhale.netlify.app/"
+        >
+          Inhale Meditation Timer{" "}
+        </a>{" "}
+        |{" "}
+        <a
+          className="transition-colors hover:text-green-600"
+          href="https://buildwithpride.org/"
+        >
+          Build With Pride{" "}
+        </a>{" "}
+        |{" "}
+        <a
+          className="transition-colors hover:text-green-600"
+          href="https://getcascara.com/"
+        >
+          Cascara{" "}
+        </a>
+      </p>
+      <div className="h-24" />
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
-import Heading from "../../components/Heading";
-import Image from "next/image";
-import asterisk from "../../public/asterisk.svg";
+import Splash from "../../components/Splash";
 export default function Home() {
   return (
     <div className="flex flex-row justify-center min-h-screen px-16">
       <div className="flex flex-col justify-between max-w-4xl pt-24">
-        <main className="">
-          <Heading />
+        <main className="border-top">
+          <Splash />
         </main>
         <footer className="py-6">
           <a className="transition-colors hover:text-green-600" href="">
             built with 🏳️‍🌈
-          </a>
+          </a>{" "}
           by murphy studebaker
         </footer>
       </div>
