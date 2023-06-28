badda599343c5a7c623ffc120f2b2ffca3565d1f
Murphy Studebaker <murphy.stude@gmail.com>
2023-06-28 14:32:06 -0700

    temporarily removed half baked code

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,15 +1,6 @@
 import notion from "../../../../lib/notion";
 
-async function getData(slug) {
-  // look up a post id using slug
-  // fetch the body of the post by id
-  // send it back to the client
-  return res
-}
-
-
 export default function Post({ params }) {
-  const data = await getData(params.slug)
   return (
     <div>
       <h1>{params.slug}</h1>
